d76efda8652ee67c5e6478491de9995eba9d2396
gigaden <gigaden@ya.ru>
2024-11-01 23:24:02 +0300

    added StatisticClient, changed response on the StatisticController go ResponseEntity<>

---
+++ statistics/client/src/main/java/StatisticClient.java
@@ -0,0 +1,58 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+import ru.practicum.dto.StatisticDtoCreate;
+import ru.practicum.dto.StatisticDtoResponse;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+
+public class StatisticClient {
+
+    private final RestClient restClient;
+
+    @Value("${statistic-server.url}")
+    private String baseUrl;
+    private final String postUri = "/hit";
+    private final String getUri = "/stats";
+
+    public StatisticClient() {
+        restClient = RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+
+    public StatisticDtoCreate createStatistic(StatisticDtoCreate statisticDtoCreate) {
+        StatisticDtoCreate statistic = restClient.post()
+                .uri(postUri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(statisticDtoCreate)
+                .retrieve()
+                .body(StatisticDtoCreate.class);
+
+        return statistic;
+    }
+
+    public Collection<StatisticDtoResponse> getStatistics(LocalDateTime start,
+                                                          LocalDateTime end,
+                                                          List<String> uris,
+                                                          boolean unique) {
+        Collection<StatisticDtoResponse> statistics = restClient.get().uri(uriBuilder -> uriBuilder
+                .path(getUri)
+                .queryParam("start", start)
+                .queryParam("end", end)
+                .queryParam("uris", uris)
+                .queryParam("unique", unique)
+                .build())
+                .header("Content-Type", "application/json")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+
+        return statistics;
+    }
+
+}
--- statistics/server/src/main/java/ru/practictum/server/controller/StatisticController.java
+++ statistics/server/src/main/java/ru/practictum/server/controller/StatisticController.java
@@ -4,7 +4,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practictum.server.service.StatisticService;
 import ru.practicum.dto.StatisticDtoCreate;
 import ru.practicum.dto.StatisticDtoResponse;
@@ -25,18 +30,18 @@ public class StatisticController {
     }
 
     @PostMapping("/hit")
-    @ResponseStatus(HttpStatus.CREATED)
-    public StatisticDtoCreate create(@Valid @RequestBody StatisticDtoCreate statistic) {
-        return service.create(statistic);
+    public ResponseEntity<StatisticDtoCreate> create(@Valid @RequestBody StatisticDtoCreate statisticDtoCreate) {
+        StatisticDtoCreate statistic = service.create(statisticDtoCreate);
+        return new ResponseEntity<>(statistic, HttpStatus.CREATED);
     }
 
 
     @GetMapping("/stats")
-    @ResponseStatus(HttpStatus.OK)
-    public Collection<StatisticDtoResponse> getAll(@RequestParam LocalDateTime start,
-                                                   @RequestParam LocalDateTime end,
-                                                   @RequestParam List<String> uris,
-                                                   @RequestParam(defaultValue = "false") boolean unique) {
-        return service.getAll(start, end, uris, unique);
+    public ResponseEntity<Collection<StatisticDtoResponse>> getAll(@RequestParam LocalDateTime start,
+                                                                   @RequestParam LocalDateTime end,
+                                                                   @RequestParam List<String> uris,
+                                                                   @RequestParam(defaultValue = "false") boolean unique) {
+        Collection<StatisticDtoResponse> statistics = service.getAll(start, end, uris, unique);
+        return new ResponseEntity<>(statistics, HttpStatus.OK);
     }
 }
